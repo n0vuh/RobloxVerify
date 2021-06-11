e967943dc41cbab183b3201cd4f5a037033f359a
novuh <56933503+n0vuh@users.noreply.github.com>
2021-06-11 16:34:00 -0400

    Support for Discord intents, more functionality
    Cleaned up code (making it more readable), added support for Discord intents & added extra functionality.

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 # made by novuh.dev/github
-
+# NEED HELP? -> https://github.com/n0vuh/RobloxVerify/README.md
+# NEED HELP? -> https://github.com/n0vuh/RobloxVerify/README.md
+# NEED HELP? -> https://github.com/n0vuh/RobloxVerify/README.md
 import discord, requests, time, random
 from discord.ext import commands
 
 BOT_TOKEN = "bot token here"
 BOT_PREFIX = "!"
-
-client = commands.Bot(command_prefix=BOT_PREFIX, help_command=None)
+MAX_REQUESTS = 20
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(command_prefix=BOT_PREFIX, help_command=None, intents=intents)
 
 @client.event
-async def on_ready(): print('Bot Online. - novuh.dev')
+async def on_ready(): 
+    print('Bot Online!')
 
 @client.command()
 async def verify(ctx):
-    await ctx.send(f'{ctx.message.author.mention}: Step 1/3 | Please enter your ROBLOX username.')
-    msg = await client.wait_for('message', check=lambda message: message.author == ctx.message.author)
-    robloxUsername = msg.content
-    robloxId = requests.get(f'https://api.roblox.com/users/get-by-username?username={msg.content}').json()['Id']
+    t = 0
+    while True:
+        await ctx.send(f'{ctx.message.author.mention}: Step 1/3 | Please enter your ROBLOX username.')
+        robloxUsername = await client.wait_for('message', check=lambda message: message.author == ctx.message.author).content
+        try:
+            robloxId = requests.get(f'https://api.roblox.com/users/get-by-username?username={robloxUsername}').json()['Id']
+            break
+        except: await ctx.send(f'{ctx.message.author.mention}: Sorry! I was unable to retrieve your user ID!')
+        
     privKey = ' '.join(random.choices([line.strip() for line in open('words.txt', 'r')], k=5))
     await ctx.send(f'{ctx.message.author.mention}: Step 2/3 | Please put the phrase `{privKey}` in your ROBLOX status. [https://i.imgur.com/qH6nvmZ.gif]')
     while True:
-        if requests.get(f'https://users.roblox.com/v1/users/{robloxId}/status').json()['status'] == privKey: break
-        else: time.sleep(3)
-    await ctx.send(f'{ctx.message.author.mention}: Step 3/3 | Thank you for verifying!')
-    # AFTER VERIFY TASKS HERE, YOU CAN CALL THEIR USERNAME WITH robloxUsername
-
+        if requests.get(f'https://users.roblox.com/v1/users/{robloxId}/status').json()['status'] == privKey: 
+            await ctx.send(f'{ctx.message.author.mention}: Step 3/3 | Thank you for verifying!')
+            break
+        else:
+            if t >= MAX_REQUESTS: 
+                await ctx.send(f"{ctx.message.author.mention}: You did not verify in time, please try again.")
+                break
+            else:
+                time.sleep(3);t+=1
+    
+    # -- YOUR CODE BELOW -- Note: you can call their username with "robloxUsername"
+    
 client.run(BOT_TOKEN)
